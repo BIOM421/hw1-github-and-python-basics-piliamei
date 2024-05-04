d64fd7214d6c514f509dc9d7e1bd615ba72e99b4
piliamei <150381842+piliamei@users.noreply.github.com>
2024-05-04 20:19:32 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 20:08:44 2024
+
+@author: pilarameijeiras
+"""
+
 def hello_world():
-	return "Hello"
+    return "Hello World!"
+
+def hello_world_n(N):
+    return " ".join(["Hello World!" for _ in range(N)])
